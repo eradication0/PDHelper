caa04af67938165a24fb612f754f37fedbe0079f
Jesse Purice <jessepurice@gmail.com>
2024-05-06 11:00:25 -0700

    Added buttons to select colors (without functionality)

--- PD Helper/ColorProfileForm.Designer.cs
+++ PD Helper/ColorProfileForm.Designer.cs
@@ -28,20 +28,265 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.attackLightColorButton = new System.Windows.Forms.Button();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.defenseLightColorButton = new System.Windows.Forms.Button();
+			this.eraseLightColorButton = new System.Windows.Forms.Button();
+			this.statusLightColorButton = new System.Windows.Forms.Button();
+			this.specialLightColorButton = new System.Windows.Forms.Button();
+			this.environmentalLightColorButton = new System.Windows.Forms.Button();
+			this.environmentalDarkColorButton = new System.Windows.Forms.Button();
+			this.specialDarkColorButton = new System.Windows.Forms.Button();
+			this.statusDarkColorButton = new System.Windows.Forms.Button();
+			this.eraseDarkColorButton = new System.Windows.Forms.Button();
+			this.defenseDarkColorButton = new System.Windows.Forms.Button();
+			this.attackDarkColorButton = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.environmentalDarkColorButton);
+			this.groupBox1.Controls.Add(this.specialDarkColorButton);
+			this.groupBox1.Controls.Add(this.statusDarkColorButton);
+			this.groupBox1.Controls.Add(this.eraseDarkColorButton);
+			this.groupBox1.Controls.Add(this.defenseDarkColorButton);
+			this.groupBox1.Controls.Add(this.attackDarkColorButton);
+			this.groupBox1.Controls.Add(this.environmentalLightColorButton);
+			this.groupBox1.Controls.Add(this.specialLightColorButton);
+			this.groupBox1.Controls.Add(this.statusLightColorButton);
+			this.groupBox1.Controls.Add(this.eraseLightColorButton);
+			this.groupBox1.Controls.Add(this.defenseLightColorButton);
+			this.groupBox1.Controls.Add(this.label8);
+			this.groupBox1.Controls.Add(this.label7);
+			this.groupBox1.Controls.Add(this.attackLightColorButton);
+			this.groupBox1.Controls.Add(this.label6);
+			this.groupBox1.Controls.Add(this.label5);
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.ForeColor = System.Drawing.SystemColors.ScrollBar;
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(179, 217);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Skill Color Picker";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(6, 186);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(84, 15);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "Environmental";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(6, 157);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(44, 15);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "Special";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(6, 128);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(39, 15);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "Status";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(6, 99);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(34, 15);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Erase";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 70);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(49, 15);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Defense";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 41);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 15);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Attack";
+			// 
+			// attackLightColorButton
+			// 
+			this.attackLightColorButton.Location = new System.Drawing.Point(96, 37);
+			this.attackLightColorButton.Name = "attackLightColorButton";
+			this.attackLightColorButton.Size = new System.Drawing.Size(23, 23);
+			this.attackLightColorButton.TabIndex = 6;
+			this.attackLightColorButton.UseVisualStyleBackColor = true;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(96, 19);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(34, 15);
+			this.label7.TabIndex = 7;
+			this.label7.Text = "Light";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(136, 19);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(31, 15);
+			this.label8.TabIndex = 8;
+			this.label8.Text = "Dark";
+			// 
+			// defenseLightColorButton
+			// 
+			this.defenseLightColorButton.Location = new System.Drawing.Point(96, 66);
+			this.defenseLightColorButton.Name = "defenseLightColorButton";
+			this.defenseLightColorButton.Size = new System.Drawing.Size(23, 23);
+			this.defenseLightColorButton.TabIndex = 9;
+			this.defenseLightColorButton.UseVisualStyleBackColor = true;
+			// 
+			// eraseLightColorButton
+			// 
+			this.eraseLightColorButton.Location = new System.Drawing.Point(96, 95);
+			this.eraseLightColorButton.Name = "eraseLightColorButton";
+			this.eraseLightColorButton.Size = new System.Drawing.Size(23, 23);
+			this.eraseLightColorButton.TabIndex = 10;
+			this.eraseLightColorButton.UseVisualStyleBackColor = true;
+			// 
+			// statusLightColorButton
+			// 
+			this.statusLightColorButton.Location = new System.Drawing.Point(96, 124);
+			this.statusLightColorButton.Name = "statusLightColorButton";
+			this.statusLightColorButton.Size = new System.Drawing.Size(23, 23);
+			this.statusLightColorButton.TabIndex = 11;
+			this.statusLightColorButton.UseVisualStyleBackColor = true;
+			// 
+			// specialLightColorButton
+			// 
+			this.specialLightColorButton.Location = new System.Drawing.Point(96, 153);
+			this.specialLightColorButton.Name = "specialLightColorButton";
+			this.specialLightColorButton.Size = new System.Drawing.Size(23, 23);
+			this.specialLightColorButton.TabIndex = 12;
+			this.specialLightColorButton.UseVisualStyleBackColor = true;
+			// 
+			// environmentalLightColorButton
+			// 
+			this.environmentalLightColorButton.Location = new System.Drawing.Point(96, 182);
+			this.environmentalLightColorButton.Name = "environmentalLightColorButton";
+			this.environmentalLightColorButton.Size = new System.Drawing.Size(23, 23);
+			this.environmentalLightColorButton.TabIndex = 13;
+			this.environmentalLightColorButton.UseVisualStyleBackColor = true;
+			// 
+			// environmentalDarkColorButton
+			// 
+			this.environmentalDarkColorButton.Location = new System.Drawing.Point(144, 182);
+			this.environmentalDarkColorButton.Name = "environmentalDarkColorButton";
+			this.environmentalDarkColorButton.Size = new System.Drawing.Size(23, 23);
+			this.environmentalDarkColorButton.TabIndex = 19;
+			this.environmentalDarkColorButton.UseVisualStyleBackColor = true;
+			// 
+			// specialDarkColorButton
+			// 
+			this.specialDarkColorButton.Location = new System.Drawing.Point(144, 153);
+			this.specialDarkColorButton.Name = "specialDarkColorButton";
+			this.specialDarkColorButton.Size = new System.Drawing.Size(23, 23);
+			this.specialDarkColorButton.TabIndex = 18;
+			this.specialDarkColorButton.UseVisualStyleBackColor = true;
+			// 
+			// statusDarkColorButton
+			// 
+			this.statusDarkColorButton.Location = new System.Drawing.Point(144, 124);
+			this.statusDarkColorButton.Name = "statusDarkColorButton";
+			this.statusDarkColorButton.Size = new System.Drawing.Size(23, 23);
+			this.statusDarkColorButton.TabIndex = 17;
+			this.statusDarkColorButton.UseVisualStyleBackColor = true;
+			// 
+			// eraseDarkColorButton
+			// 
+			this.eraseDarkColorButton.Location = new System.Drawing.Point(144, 95);
+			this.eraseDarkColorButton.Name = "eraseDarkColorButton";
+			this.eraseDarkColorButton.Size = new System.Drawing.Size(23, 23);
+			this.eraseDarkColorButton.TabIndex = 16;
+			this.eraseDarkColorButton.UseVisualStyleBackColor = true;
+			// 
+			// defenseDarkColorButton
+			// 
+			this.defenseDarkColorButton.Location = new System.Drawing.Point(144, 66);
+			this.defenseDarkColorButton.Name = "defenseDarkColorButton";
+			this.defenseDarkColorButton.Size = new System.Drawing.Size(23, 23);
+			this.defenseDarkColorButton.TabIndex = 15;
+			this.defenseDarkColorButton.UseVisualStyleBackColor = true;
+			// 
+			// attackDarkColorButton
+			// 
+			this.attackDarkColorButton.Location = new System.Drawing.Point(144, 37);
+			this.attackDarkColorButton.Name = "attackDarkColorButton";
+			this.attackDarkColorButton.Size = new System.Drawing.Size(23, 23);
+			this.attackDarkColorButton.TabIndex = 14;
+			this.attackDarkColorButton.UseVisualStyleBackColor = true;
 			// 
 			// ColorProfileForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.FromArgb(33, 33, 33);
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.groupBox1);
 			this.Name = "ColorProfileForm";
-			this.Text = "ColorProfileForm";
+			this.Text = "Color Profile Settings";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private GroupBox groupBox1;
+		private Label label1;
+		private Label label6;
+		private Label label5;
+		private Label label4;
+		private Label label3;
+		private Label label2;
+		private Button environmentalDarkColorButton;
+		private Button specialDarkColorButton;
+		private Button statusDarkColorButton;
+		private Button eraseDarkColorButton;
+		private Button defenseDarkColorButton;
+		private Button attackDarkColorButton;
+		private Button environmentalLightColorButton;
+		private Button specialLightColorButton;
+		private Button statusLightColorButton;
+		private Button eraseLightColorButton;
+		private Button defenseLightColorButton;
+		private Label label8;
+		private Label label7;
+		private Button attackLightColorButton;
 	}
 }
